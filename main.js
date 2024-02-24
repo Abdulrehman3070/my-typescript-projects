ca0acb9a37e138174a725cca6862fbf54709b31c
Abdul Rehman Khan <159823236+Abdulrehman3070@users.noreply.github.com>
2024-02-24 23:55:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var personname = "Abdul Rafay khan";
+var message = "Hi ".concat(personname, ", Congragulations on 1000 subcribers");
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let personname: string = "Abdul Rafay khan"
+let message: string = `Hi ${personname}, Congragulations on 1000 subcribers`
+console.log(message)
